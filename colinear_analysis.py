9ece6d795b2bd8fb3e1f251deb0a38bb7045497d
ChenZicong <40140572+ChenZicong@users.noreply.github.com>
2022-09-25 22:18:28 +0800

    Create colinear_analysis.py

---
+++ colinear_analysis.py
@@ -0,0 +1,28 @@
+import pyspark.mllib.stat as st
+import statsmodels.stats.outliers_influence import variance_inflation_factor
+
+# 共线性分析-CORR
+# corrsMatrix = st.Statistics.corr(trainDataWOE.select(iv_var_list).rdd.map(lambda row: [e for e in row]))
+
+corrsMatrix = trainDataWOE[iv_var_list].corr(method='pearson').values
+del_var_list = []
+for i in range(corrsMatrix.shape[0]-1):
+     for j in range(i+1, corrsMatrix.shape[0]):
+          if corrsMatrix[i,j] >= 0.7:
+               v1 = iv_var_list[i]
+               v2 = iv_var_list[j]
+               del_var = v2 if IV_dict[v1[:-4]]>IV_dict[v2[:-4]] else v1
+               print(v1, v2, round(corrsMatrix[i,j], 2), '==> delete var:', del_var)
+               del_var_list.append(del_var)
+del_var_list = list(set(del_var_list))
+select_var = [i for i in iv_var_list if i not in del_var_list]
+
+
+# 共线性分析-VIF
+
+X = np.matrix(trainDataWOE[select_var].toPandas())
+VIF_list = [variance_inflation_factor(X,i) for i in range(X.shape[1])]
+VIF_dict = {}
+for i, j in zip(select_var, VIF_list):
+     VIF_dict[i] = j
+sorted(VIF_dict.items(), key=lambda x: x[1], reverse=True)
