d435ee3327dbcfb59928bc0bc805a795cbb3a829
ChenZicong <40140572+ChenZicong@users.noreply.github.com>
2023-03-05 19:51:42 +0800

    Update LSTM+LR.py

--- DeepLearning/LSTM+LR.py
+++ DeepLearning/LSTM+LR.py
@@ -1,6 +1,12 @@
 
-# 
-train_x = data_x.values.reshape((-1,6,16)
-train_y = data_y.values
-test_x = data_x.values.reshape((-1,6,16))
-test_y = data_y.values
+# train_data_ts 为时间序列数据，6个通道，16个日期
+train_ts = train_data_ts.values.reshape((-1,6,16)
+test_ts = test_data_ts.values.reshape((-1,6,16))
+                                       
+# train_data_var 为各类训练特征
+train_var = train_data_var.values
+test_var = test_data_var.values
+                                            
+# 预测目标Y
+train_y = train_data_y.values
+test_y = test_data_y.values
